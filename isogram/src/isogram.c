4c97637a9460ed2b8fc8241b2da4a49229a7e9f1
kiki <thiphan94@gmail.com>
2019-05-31 16:15:10 +0200

    update

--- isogram/src/isogram.c
+++ isogram/src/isogram.c
@@ -7,12 +7,19 @@ bool is_isogram(const char phrase[]) {
   if (phrase == 0) {
     return false;
   }
-  for (int i = 0; phrase[i] != '\0'; i++) {
-    for (int j = i + 1; phrase[j] != '\0'; j++) {
-      if (isalpha(phrase[j]) && tolower(phrase[j]) == tolower(phrase[i])) {
+
+  int n = strlen(phrase);
+  int map[26] = {0};
+
+  for (int i = 0; i < n; i++) {
+    if (phrase[i] != ' ' && phrase[i] != '-') {
+      int index = tolower(phrase[i]) - 'a';
+      map[index]++;
+      if (map[tolower(phrase[i]) - 'a'] > 1) {
         return false;
       }
     }
   }
+
   return true;
 }
