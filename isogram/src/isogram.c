c8673bb3cf5eb68e3a211fc42a6b4eea09e440ca
kiki <thiphan94@gmail.com>
2019-06-19 01:05:47 +0200

    update

--- isogram/src/isogram.c
+++ isogram/src/isogram.c
@@ -2,6 +2,7 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+#define NUMBER_LETTRES 26
 
 bool is_isogram(const char phrase[]) {
   if (phrase == 0) {
@@ -9,13 +10,13 @@ bool is_isogram(const char phrase[]) {
   }
 
   int n = strlen(phrase);
-  int map[26] = {0};
+  int map[NUMBER_LETTRES] = {0};
 
   for (int i = 0; i < n; i++) {
     if (phrase[i] != ' ' && phrase[i] != '-') {
       int index = tolower(phrase[i]) - 'a';
       map[index]++;
-      if (map[tolower(phrase[i]) - 'a'] > 1) {
+      if (map[index] > 1) {
         return false;
       }
     }
